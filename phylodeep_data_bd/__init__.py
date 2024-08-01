7e59301e407d71c1545ccf6392d18ce7c21414ed
Anna Zhukova <zhutchok@gmail.com>
2024-08-01 17:04:15 +0200

    v0.5

--- phylodeep_data_bd/__init__.py
+++ phylodeep_data_bd/__init__.py
@@ -7,17 +7,26 @@ TARGET_NAMES = ["R_naught", "Infectious_period", 'sampling_proba', 'tree_size']
 PREDICTED_NAMES = ["R_naught", "Infectious_period"]
 
 
+CNN_FULL_TREE = 'CNN_FULL_TREE'
+FFNN_SUMSTATS = 'FFNN_SUMSTATS'
+
+LARGE = 'LARGE' # 200 <= #tips <= 500
+SMALL = 'SMALL' # 50 <= #tips < 200
+
+ALLOWED_TREE_SIZES = (SMALL, LARGE)
+ALLOWED_ENCODINGS = (CNN_FULL_TREE, FFNN_SUMSTATS)
+
+
+
 PREFIX = os.path.abspath(os.path.dirname(__file__))
 
 
-def get_ci_tables(tree_size, encoding):
+def get_ci_tables(encoding, tree_size, **kwargs):
     """
     Loads the tables required for CI computation (for approximated parametric bootstrap)
 
-    :param encoding: str, 'FFNN_SUMSTATS' or 'CNN_FULL_TREE'
-
-    :param tree_size: str, 'LARGE' or 'SMALL', corresponding to the size of the tree
-        ('SMALL', if 49<#tips<200; 'LARGE', if 199<#tips<501)
+    :param encoding: str, one of ALLOWED_ENCODINGS
+    :param tree_size: str, the ALLOWED_TREE_SIZES
 
     :return: two pd.DataFrame, containing values for CI computation: predicted and target tables
     """
@@ -26,19 +35,22 @@ def get_ci_tables(tree_size, encoding):
             pd.read_csv(target_path, compression='xz', header=None, names=TARGET_NAMES))
 
 
-def get_ci_table_paths(tree_size, encoding):
+def get_ci_table_paths(encoding, tree_size, **kwargs):
     """
     Returns the paths to the tables required for CI computation (for approximated parametric bootstrap)
 
-    :param encoding: str, 'FFNN_SUMSTATS' or 'CNN_FULL_TREE'
-
-    :param tree_size: str, 'LARGE' or 'SMALL', corresponding to the size of the tree
-        ('SMALL', if 49<#tips<200; 'LARGE', if 199<#tips<501)
+    :param encoding: str, one of ALLOWED_ENCODINGS
+    :param tree_size: str, the ALLOWED_TREE_SIZES
 
     :return: tuple containing the paths to the tables containing values for CI computation: predicted and target
     """
-    return (os.path.join(PREFIX, tree_size.lower(), '{}.csv.xz'.format(encoding)),
-            os.path.join(PREFIX, tree_size.lower(), 'target.csv.xz'))
+    if tree_size not in ALLOWED_TREE_SIZES:
+        raise ValueError('Tree size must be one of: {}'.format(', '.join(ALLOWED_TREE_SIZES)))
+    if encoding not in ALLOWED_ENCODINGS:
+        raise ValueError('Encoding must be one of: {}'.format(', '.join(ALLOWED_ENCODINGS)))
+    tree_size = tree_size.lower()
+    return (os.path.join(PREFIX, tree_size, '{}.csv.xz'.format(encoding)),
+            os.path.join(PREFIX, tree_size, 'target.csv.xz'))
 
 
 def main():
@@ -53,12 +65,12 @@ def main():
                                      prog='bd_ci_paths')
 
     parser.add_argument('-s', '--tree_size',
-                        help="input tree size, can be 'LARGE' (200-500 tips) or 'SMALL' (50-199 tips).",
-                        type=str, required=True, choices=('LARGE', 'SMALL'), default='LARGE')
+                        help="input tree size",
+                        type=str, required=True, choices=ALLOWED_TREE_SIZES, default=LARGE)
 
-    parser.add_argument('-e', '--encoding', help="'FFNN_SUMSTATS' or 'CNN_FULL_TREE'",
-                        type=str, required=True, choices=('FFNN_SUMSTATS', 'CNN_FULL_TREE'),
-                        default='CNN_FULL_TREE')
+    parser.add_argument('-e', '--encoding', help="input tree encoding",
+                        type=str, required=True, choices=ALLOWED_ENCODINGS,
+                        default=CNN_FULL_TREE)
 
     params = parser.parse_args()
 
